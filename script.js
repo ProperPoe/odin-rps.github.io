074b64b23bc9ae6a5383557286b5963610a910ee
Potency <proper@ProperPoe>
2023-08-11 16:43:31 -0600

    Added a boolean flag, and also put the addEvenListener on the form itself
    The boolean flag is used as a condition that if met, the game stops. The event listener is now on the form instead of the button.

--- script.js
+++ script.js
@@ -11,64 +11,87 @@ let clickStop = 0;
 let playScore = 0;
 let compScore = 0;
 
+let gameActive = true;
+
 let rpsArray = ["Rock", "Paper", "Scissors"]
 
 
 
 const computerChoice = (ind) =>{
     return rpsArray[ind]
-}
+}  
 
-//while(playScore !== 5){
-    function game(e){
-        
-            e.preventDefault();
-            
-            const ind = Math.floor(Math.random() * 3);
-            let val = theInput.value;
-            let hmm = computerChoice(ind);
-            player.innerHTML = val
-            computer.innerHTML = hmm
-            //player.appendChildChild(playerChoice);
-            //computer.appendChild(compChoice);
-            if(val === "Rock" && computerChoice(ind) === "Scissors"){
-                playScore += 1
-                pScore.innerHTML = playScore
-            }else if(val === "Scissors" && computerChoice(ind) === "Rock"){
-                compScore += 1
-                cScore.innerHTML = compScore;
-            }
-            if(val === "Paper" && computerChoice(ind) === "Rock"){
-                playScore += 1
-                pScore.innerHTML = playScore
-            }else if(val === "Rock" && computerChoice(ind) === "Paper"){
-                compScore += 1
-                cScore.innerHTML = compScore;
-            }
-            if(val === "Scissors" && computerChoice(ind) === "Paper"){
-                playScore += 1
-                pScore.innerHTML = playScore
-            }else if(val === "Paper" && computerChoice(ind) === "Scissors"){
-                compScore += 1
-                cScore.innerHTML = compScore;
-            }
-            
-            if(playScore === 5){
-                result.innerHTML = "You win!"
-                pScore.innerHTML = 0
-            }else if(compScore === 5){
-                result.innerHTML = "Computer wins"
-                cScore.innerHTML = 0
-            }
-            
+theForm.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    if(gameActive){
+        game()
     }
-            
-//}
-            
+    
+})  
+
+const game = () => {
         
+    //e.preventDefault();
+    
+    const ind = Math.floor(Math.random() * 3);
+    let val = theInput.value;
+    let hmm = computerChoice(ind);
+    let gameOver = false;
+    player.innerHTML = val
+    computer.innerHTML = hmm
+   
+    //player.appendChildChild(playerChoice);
+    //computer.appendChild(compChoice);
+    //while(playScore < 5 || compScore < 5){  
+    if(val){
+        if(val === "Rock" && computerChoice(ind) === "Scissors"){
+            playScore += 1
+            pScore.innerHTML = playScore
+        }else if(val === "Scissors" && computerChoice(ind) === "Rock"){
+            compScore += 1
+            cScore.innerHTML = compScore;
+        }
+        if(val === "Paper" && computerChoice(ind) === "Rock"){
+            playScore += 1
+            pScore.innerHTML = playScore
+        }else if(val === "Rock" && computerChoice(ind) === "Paper"){
+            compScore += 1
+            cScore.innerHTML = compScore;
+        }
+        if(val === "Scissors" && computerChoice(ind) === "Paper"){
+            playScore += 1
+            pScore.innerHTML = playScore
+        }else if(val === "Paper" && computerChoice(ind) === "Scissors"){
+            compScore += 1
+            cScore.innerHTML = compScore;
+        }
+        
+        if(playScore === 5){
+            result.innerHTML = "You win!"
+            gameActive = false
+        }else if(compScore === 5){
+            result.innerHTML = "Computer wins"
+            gameActive = false
+        }
+    }  
     
 
-btn.addEventListener("click", game )
+    theInput.value = "";
+}
+    
+            
+        //}
+            
+          
+        
+
+
+
+
+
+
+
+
 
 
 
