dcfa6dc6d4757ab10a268325811359a03e90260b
Potency <proper@ProperPoe>
2023-08-11 13:49:19 -0600

    A little more styling on rendering sections

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 const theForm = document.getElementById("form");
 const theInput = document.querySelector("input");
 const btn = document.getElementById("btn")
-const player = document.getElementById("pChoice");
-const computer = document.getElementById("cChoice");
+let player = document.getElementById("pChoice");
+let computer = document.getElementById("cChoice");
 
 let clickStop = 0;
 
 let rpsArray = ["Rock", "Paper", "Scissors"]
 
+
+
 const computerChoice = (ind) =>{
     return rpsArray[ind]
 }
 
-btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const ind = Math.floor(Math.random() * 3);
-    console.log(`The computer chose ${computerChoice(ind)}`)
-    let val = theInput.value;
-    let playerChoice = document.createElement("h1");
-    let compChoice = document.createElement("h1");
-    while(clickStop < 1){
-        clickStop += 1
-        playerChoice.innerHTML = val;
-        compChoice.innerHTML = computerChoice(ind);
-        player.appendChild(playerChoice);
-        computer.appendChild(compChoice);
-        if(val === "Rock" && computerChoice(ind) === "Scissors"){
-        console.log("You win!")
-    }
-    }
+//while(clickStop < 4){
+    function game(e){
+        
+            e.preventDefault();
+            player.innerHTML = ""
+            computer.innerHTML = ""
+            const ind = Math.floor(Math.random() * 3);
+            console.log(`The computer chose ${computerChoice(ind)}`)
+            let val = theInput.value;
+            let playerChoice = document.createElement("h1");
+            let compChoice = document.createElement("h1");
+            
+            
+            let whoa = playerChoice.innerHTML = val;
+            let hmm = computerChoice(ind);
+            player.innerHTML = whoa
+            computer.innerHTML = hmm
+            //player.appendChildChild(playerChoice);
+            //computer.appendChild(compChoice);
+            if(val === "Rock" && computerChoice(ind) === "Scissors"){
+                console.log("You win!")
+            }
+            }
+            
+            clickStop += 1
+            
+        
     
-})
+
+btn.addEventListener("click", game )
+
 
 
 
