0be433f90d9c380908965c7b5c549154cd44bf3d
Potency <proper@ProperPoe>
2023-08-11 12:48:18 -0600

    Connected the empty elements in html file with javascript to render
    I created two elements in html that shows what the player and computer chose, and displays the text of what they chose. I connected those elements to javascript, and now when the player chooses an action, it renders it in the empty element, aswell as the computers action

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const theForm = document.getElementById("form");
 const theInput = document.querySelector("input");
 const btn = document.getElementById("btn")
+const player = document.getElementById("pChoice");
+const computer = document.getElementById("cChoice");
 
-
+let clickStop = 0;
 
 let rpsArray = ["Rock", "Paper", "Scissors"]
 
@@ -15,9 +17,19 @@ btn.addEventListener("click", (e) => {
     const ind = Math.floor(Math.random() * 3);
     console.log(`The computer chose ${computerChoice(ind)}`)
     let val = theInput.value;
-    if(val === "Rock" && computerChoice(ind) === "Scissors"){
+    let playerChoice = document.createElement("h1");
+    let compChoice = document.createElement("h1");
+    while(clickStop < 1){
+        clickStop += 1
+        playerChoice.innerHTML = val;
+        compChoice.innerHTML = computerChoice(ind);
+        player.appendChild(playerChoice);
+        computer.appendChild(compChoice);
+        if(val === "Rock" && computerChoice(ind) === "Scissors"){
         console.log("You win!")
     }
+    }
+    
 })
 
 
